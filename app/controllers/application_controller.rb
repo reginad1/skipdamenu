6716b2a69b1c688df5de3c00d494f7a5e09b00bb
Apphia Mann & Dayne Beck & Joseph Kim & Regina DeAngelis <pair+apphiamann.dbeck28.josekim.reginad1@devbootcamp.com>
2017-08-17 17:31:58 -0500

    Foursquare api calls available

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  require 'lib/foursquare'
+  include Foursquare
   protect_from_forgery with: :exception
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
+require 'oauth2'
 module ApplicationHelper
+  def self.get_client
+    client = Foursquare2::Client.new(:client_id => ENV["foursq_client"], :client_secret => ENV["foursq_sec"],:api_version => '20170817')
+  end
 end
