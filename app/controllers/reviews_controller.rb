6c55cb61d7bfdee967bd9198fb2efe05f17dbf55
Apphia Mann & Regina DeAngelis <pair+apphiamann.reginad1@devbootcamp.com>
2017-08-20 15:22:23 -0500

    yay review form for items

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+	def index
+		@reviews = Review.all
+	end 
+
+	def new 
+		@review = Review.new
+	end 
+
+	def create
+		@review = Review.new(review_params)
+
+		respond_to do |format|
+			if @review.save
+				format.html {redirect_to @review, notice: 'Review was successfully created.'}
+				format.json {render :show, status: :created, location: @review }
+			else 
+				format.html {render :new }
+				format.json {render json: @review.errors, status: :unprocessable_entity}
+			end 
+		end 
+	end 
 end
