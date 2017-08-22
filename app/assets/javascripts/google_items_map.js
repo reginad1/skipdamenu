8a7debcd82e8750bc96787e21972a87df1bca1a1
Joseph Kim <email": null>
2017-08-22 17:48:50 -0500

    fix map bug

--- app/assets/javascripts/google_items_map.js
+++ app/assets/javascripts/google_items_map.js
@@ -6,14 +6,14 @@ $( document ).ready(function() {
 
 
 function map_init_item(place_id) {
-  var map = new google.maps.Map(document.getElementById('map-container2'), {
+  let map = new google.maps.Map(document.getElementById('map-container2'), {
     center: place_id,
     zoom: 11
   });
 
-  var infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  var image = {
+  let infowindow = new google.maps.InfoWindow();
+  let service = new google.maps.places.PlacesService(map);
+  let image = {
   url: 'http://maps.google.com/mapfiles/kml/pal2/icon32.png',
   // This marker is 20 pixels wide by 32 pixels high.
   size: new google.maps.Size(32, 32),
@@ -22,15 +22,18 @@ function map_init_item(place_id) {
   // The anchor for this image is the base of the flagpole at (0, 32).
   anchor: new google.maps.Point(0, 32)
   };
- for(var i = 0; i < restaurants.length; i++) {
-  var this_rest = new google.maps.LatLng(restaurants[i].latitude,restaurants[i].longitude);
-  var resName = restaurants[i].name
-  var items = filter_items(item_data, restaurants[i].id)
-  var marker = new google.maps.Marker({
+  for(let i = 0; i < restaurants.length; i++) {
+    restaurants[i].fitems = filter_items(item_data, restaurants[i].id)
+  }
+ for(let i = 0; i < restaurants.length; i++) {
+  let this_rest = new google.maps.LatLng(restaurants[i].latitude,restaurants[i].longitude);
+  let resName = restaurants[i].name
+  let item = restaurants[i].fitems
+  let marker = new google.maps.Marker({
     map: map,
     position: this_rest,
     title: resName,
-    item: items,
+    item: item,
     icon: image,
     item_url: `/items/${item_data[i].id}`,
     rest_url: `/restaurants/${restaurants[item_data[i].restaurant_id].id}`
@@ -46,7 +49,7 @@ function filter_items(item_data,restaurantsId) {
 }
 
 function add_markers(marker, infowindow, map) {
-  items =""
+  let items =""
   for(let y = 0; y < marker.item.length; y++) {
     items += `<div><a href="/items/${marker.item[y].id}"> ${marker.item[y].name}  </a></div>`
   }
