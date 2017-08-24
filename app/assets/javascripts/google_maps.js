65271e801f12789ccbd6339fb34888efda1e24f7
Regina DeAngelis <reginadeangelis91@gmail.com>
2017-08-24 14:47:52 -0500

    Add cloud key to env

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,8 +1,8 @@
 function getGoogleID(search_term) {
   var map = new google.maps.Map(document.createElement('div'), {
-      center: this_rest,
-      zoom: 15
-    });
+    center: this_rest,
+    zoom: 15
+  });
 
   var request = {
     location: this_rest,
@@ -14,27 +14,30 @@ function getGoogleID(search_term) {
   var service = new google.maps.places.PlacesService(map);
   service.textSearch(request, callback);
   function callback(result) {
-    result = result.shift();
-    //make an ajax call -> update our restaurauasdiaser.google_id with place_id
-    updateGoogleID(result.place_id,'/restaurants/'+Rid)
-    map_init(result.place_id) // this is technically our google_id
+    if (result.length > 0) {
+      result = result.shift();
+      //make an ajax call -> update our restaurauasdiaser.google_id with place_id
+      console.log(result)
+      updateGoogleID(result.place_id,'/restaurants/'+Rid)
+      map_init(result.place_id) // this is technically our google_id
     }
-      }
+  }
+}
 
 function map_init(place_id) {
-        var map = new google.maps.Map(document.getElementById('map-container'), {
-          center: this_rest,
-          zoom: 15
-        });
+  var map = new google.maps.Map(document.getElementById('map-container'), {
+    center: this_rest,
+    zoom: 15
+  });
 
-        var infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
+  var infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
 
-        service.getDetails({
-          placeId: place_id
-        },
-         function callback(result, status) {
-          updateRestaurantInfo(result)
+  service.getDetails({
+    placeId: place_id
+  },
+  function callback(result, status) {
+    updateRestaurantInfo(result)
 
           ///// Its placing the Marker///////
           if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -44,21 +47,21 @@ function map_init(place_id) {
             });
             google.maps.event.addListener(marker, 'click', function() {
               infowindow.setContent('<div><strong>' + result.name + '</strong><br>' +
-              result.formatted_address + '</div>');
+                result.formatted_address + '</div>');
               infowindow.open(map, this);
             });
           }
         });
-      }
+}
 
 $( document ).ready(function() {
-    if($('#map-container').length >= 1){
-      if (google_id == "")  {
-        getGoogleID(resName)
-      } else  {
-        map_init(google_id)
-      }
+  if($('#map-container').length >= 1){
+    if (google_id == "")  {
+      getGoogleID(resName)
+    } else  {
+      map_init(google_id)
     }
+  }
 
 
 });
