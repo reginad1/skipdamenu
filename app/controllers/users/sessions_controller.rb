88cb6a4f7f6ca8dc3d0a26559a31b53bb22d9f12
Apphia Mann & Regina DeAngelis <pair+apphiamann.reginad1@devbootcamp.com>
2017-08-24 10:36:46 -0500

    css

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  # before_action :redirect, only: [:new,:create]
 
   # GET /resource/sign_in
   # def new
@@ -21,5 +21,10 @@ class Users::SessionsController < Devise::SessionsController
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+  # def redirect
+  #   if current_user.id ==  nil
+  #     redirect_to "/"
+  #   end
   # end
 end
